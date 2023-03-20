c510ee5a7fa0face4867a64df14626fe1c32388f
rodolfobomm <111303282+rodolfobomm@users.noreply.github.com>
2023-03-20 20:24:19 -0300

    Add files via upload

---
+++ src/VolumeLataDeOleo.java
@@ -0,0 +1,10 @@
+public class VolumeLataDeOleo {
+    public static void main(String[] args) {
+        double raio = 5.0; // Atribuindo valor
+        double altura = 10.0; // Atribuindo valor
+
+        double volume = 3.14159 * raio * raio * altura; // Cálculo
+
+        System.out.println("O volume da lata de óleo é: " + volume); // Exibindo o resultado na tela
+    }
+}
